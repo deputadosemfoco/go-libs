cbb5d394ca7c26dad81b87eae506d4332acce7d0
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2016-09-14 19:51:47 -0300

    more base structs

--- messages/base.go
+++ messages/base.go
@@ -12,4 +12,41 @@ type (
 		ValidationMessages []string    `json:"validationMessages"`
 		Data               interface{} `json:"data"`
 	}
+
+	// ResponseBuilder is a builder for Response struct
+	ResponseBuilder struct{ Response }
+
+	// Error is the base error struct to http responses
+	Error struct {
+		Message            string `json:"message"`
+		ValidationMessages string `json:"validationMessages"`
+	}
 )
+
+func ResBuilder() *ResponseBuilder {
+	return &ResponseBuilder{}
+}
+
+func (builder *ResponseBuilder) AsSuccess(data interface{}) *ResponseBuilder {
+	builder.Success = true
+	builder.Data = data
+
+	return builder
+}
+
+func (builder *ResponseBuilder) AsErr(msg string) *ResponseBuilder {
+	builder.Success = false
+	builder.Message = msg
+
+	return builder
+}
+
+func (builder *ResponseBuilder) WithValMsgs(msgs []string) *ResponseBuilder {
+	builder.ValidationMessages = msgs
+
+	return builder
+}
+
+func (builder *ResponseBuilder) Build() Response {
+	return builder.Response
+}
